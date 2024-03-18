fd9900f30bce343c653eee88aeaece9bc3074cf2
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-03-18 20:50:35 +0530

    header thik kiya

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,55 +33,59 @@ const Header = () => {
               alignItems: "center",
             }}
           >
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <img src={amazon} style={{width:"30%"}}/>
-           
-              <div className="input-group " style={{width:"60%"}}>
-                <FaSearch
-                  className="searchIcon"
-                  style={{
-                    position: "relative",
-                    left: "12%",
-                    "z-index": "5",
-                    bottom: "-10px",
-                  }}
-                />{" "}
-                {/* Search icon */}
-                <input
-                  type="text"
-                  className="form-control border-left-0 in"
-                  placeholder="Search by prouct color,size etc,(eg:skirt,jeans...)"
-                  style={{
-                    borderRadius: "14px",
-                   paddingLeft:"14%"
-                  }}
-                />
-              </div>
-            
+            <Navbar.Toggle
+              className="custom-toggler "
+              aria-controls="basic-navbar-nav"
+            />
+            <span className="navbar-toggler-icon"></span>
+            <img src={amazon} style={{ width: "30%" }} />
+
+            <div className="input-group " style={{ width: "60%" }}>
+              <FaSearch
+                className="searchIcon"
+                style={{
+                  position: "relative",
+                  left: "12%",
+                  "z-index": "5",
+                  bottom: "-10px",
+                }}
+              />{" "}
+              {/* Search icon */}
+              <input
+                type="text"
+                className="form-control border-left-0 in"
+                placeholder="Search by prouct color,size etc,(eg:skirt,jeans...)"
+                style={{
+                  borderRadius: "14px",
+                  paddingLeft: "14%",
+                }}
+              />
+            </div>
           </Col>
-          <Col xs={12} sm={6} md={6} lg={6} style={{marginLeft:"0",fontSize:"18px"}}>
+          <Col
+            xs={12}
+            sm={6}
+            md={6}
+            lg={6}
+            style={{ marginLeft: "0", fontSize: "18px" }}
+          >
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav
                 className="mr-auto"
                 style={{
-                  
                   color: "white !important",
                   justifyContent: "space-around",
-                 
-                  alignItems:"center"
+
+                  alignItems: "center",
                 }}
               >
                 <Nav.Link href="#home">
                   <FaPhoneAlt />
                 </Nav.Link>
-                
-                <Nav.Link href="#link" >
-                
-                  Need help?
-                 
-                </Nav.Link>
+
+                <Nav.Link href="#link">Need help?</Nav.Link>
                 <Nav.Link href="#home">+912 345 667 876</Nav.Link>
-                <Nav.Link >
+                <Nav.Link>
                   <img
                     style={{ width: "25px", borderRadius: "50%" }}
                     src="https://knowindia.india.gov.in/assets/images/national_flag_inner.jpg"
@@ -113,28 +117,6 @@ const Header = () => {
         </Row>
       </Container>
     </Navbar>
-
-    /* <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar> */
   );
 };
 
